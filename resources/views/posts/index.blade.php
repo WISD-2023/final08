6281ccbdc7886766b7e2442345b8c5b1d440471d
鄭哲丞 <a0968518713@gmail.com>
2023-12-15 11:12:23 +0800

    BLOG文章修改

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 
-@section('page-title', 'Blog Home')
+@section('page-title', 'Blog文章')
 
 @section('page-style')
     <!-- Core theme CSS (includes Bootstrap)-->
@@ -17,18 +17,7 @@
             <!-- Blog entries-->
             <div class="col-lg-8">
                 <!-- Featured blog post-->
-                <div class="card mb-4">
-                    <a href="#!"><img class="card-img-top" src="https://dummyimage.com/850x350/dee2e6/6c757d.jpg"
-                                      alt="..."/></a>
-                    <div class="card-body">
-                        <div class="small text-muted">January 1, 2022</div>
-                        <h2 class="card-title">Featured Post Title</h2>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
-                            aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi
-                            vero voluptate voluptatibus possimus, veniam magni quis!</p>
-                        <a class="btn btn-primary" href="#!">Read more →</a>
-                    </div>
-                </div>
+
                 <!-- Nested row for non-featured blog posts-->
                 <div class="row">
                     @foreach($posts->chunk(2) as $chunks)
@@ -37,7 +26,6 @@
                         <!-- Blog post-->
                         <div class="card mb-4">
                             <a href="{{ route('posts.show', $post->id) }}">
-                                <img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..."/>
                             </a>
                             <div class="card-body">
                                 <div class="small text-muted">{{ $post->updated_at->diffForHumans() }}</div>
@@ -80,35 +68,7 @@
                         </div>
                     </div>
                 </div>
-                <!-- Categories widget-->
-                <div class="card mb-4">
-                    <div class="card-header">Categories</div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <ul class="list-unstyled mb-0">
-                                    <li><a href="#!">Web Design</a></li>
-                                    <li><a href="#!">HTML</a></li>
-                                    <li><a href="#!">Freebies</a></li>
-                                </ul>
-                            </div>
-                            <div class="col-sm-6">
-                                <ul class="list-unstyled mb-0">
-                                    <li><a href="#!">JavaScript</a></li>
-                                    <li><a href="#!">CSS</a></li>
-                                    <li><a href="#!">Tutorials</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Side widget-->
-                <div class="card mb-4">
-                    <div class="card-header">Side Widget</div>
-                    <div class="card-body">You can put anything you want inside of these side widgets. They are easy to
-                        use, and feature the Bootstrap 5 card component!
-                    </div>
-                </div>
+
             </div>
         </div>
     </div>
