6489076feb778d822aa3838d946ec7d03fbebf08
鄭哲丞 <a0968518713@gmail.com>
2023-12-15 11:07:36 +0800

    修改首頁

--- resources/views/home/shared/header.blade.php
+++ resources/views/home/shared/header.blade.php
@@ -3,10 +3,9 @@
     <div class="container px-lg-5">
         <div class="p-4 p-lg-5 bg-light rounded-3 text-center">
             <div class="m-4 m-lg-5">
-                <h1 class="display-5 fw-bold">A warm welcome!</h1>
-                <p class="fs-4">Bootstrap utility classes are used to create this jumbotron since the old component has
-                    been removed from the framework. Why create custom CSS when you can use utilities?</p>
-                <a class="btn btn-primary btn-lg" href="#!">Call to action</a>
+                <h1 class="display-5 fw-bold">Welcome!</h1>
+                <p class="fs-4">歡迎來到本部落格</p>
+
             </div>
         </div>
     </div>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,7 +9,6 @@
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}"/>
     <!-- Bootstrap icons-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet"/>
     @yield('page-style')
 </head>
 <body>
