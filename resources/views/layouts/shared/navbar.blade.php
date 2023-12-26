028e8292ee91d11a1668e3aa71c84c3a09ea968c
鄭哲丞 <a0968518713@gmail.com>
2023-12-26 21:57:06 +0800

    登入按鈕設定

--- resources/views/layouts/shared/navbar.blade.php
+++ resources/views/layouts/shared/navbar.blade.php
@@ -6,26 +6,24 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item"><a class="nav-link{{ (request()->is('posts*'))? " active" : "" }}" aria-current="page" href="{{ route('posts.index') }}">Blog文章</a></li>
+                <li class="nav-item">
                 @if (Route::has('login'))
-                        @auth
-                        <li class="nav-item">
+                    @auth
+                        <li><a class="nav-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();" style="color:white">{{ __('登出') }}</a>
+                        </li>
 
-                        <form method="POST" action="{{ route('logout') }}">
-                                @csrf
-
-                                <a href="{{ route('logout') }}"
-                                                 onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                    {{ __('Log Out') }}
-                                </a>
-                            </form></li>
-                        @else
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    @else
                             <a href="{{ route('login') }}" class="nav-link">登入</a></li>
                     <li class="nav-item">
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}" class="nav-link">註冊</a></li>
-                            @endif
-                        @endauth
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="nav-link">註冊</a></li>
+                    @endif
+                    @endauth
                     </div>
                 @endif
             </ul>
